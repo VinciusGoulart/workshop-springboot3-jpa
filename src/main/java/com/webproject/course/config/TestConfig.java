17ec94bd222622cc874273e8c995b41bb823d50f
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-04-24 11:07:09 -0300

    Implemented OrderStats enum

--- src/main/java/com/webproject/course/config/TestConfig.java
+++ src/main/java/com/webproject/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.webproject.course.config;
 
 import com.webproject.course.entities.Order;
 import com.webproject.course.entities.User;
+import com.webproject.course.entities.enums.OrderStatus;
 import com.webproject.course.repositories.OrderRepository;
 import com.webproject.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Venus", "venus@email.com", "88888888", "987654321");
         User u2 = new User(null, "Earth", "earth@email.com", "77777777", "789456123");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.SHIPPED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.CANCELED, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
